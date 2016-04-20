4972785bb0e259750084be5452e97371e03303f6
Gendai <t.brenet@orange.fr>
2016-04-20 12:21:18 +0100

    MileStone02

--- app/src/main/java/com/example/english/gps_tracker/CustomView.java
+++ app/src/main/java/com/example/english/gps_tracker/CustomView.java
@@ -2,22 +2,27 @@ package com.example.english.gps_tracker;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.view.View;
+import android.widget.LinearLayout;
 
 
-public class CustomView extends View {
+public class CustomView extends LinearLayout {
 
     public CustomView(Context context) {
         super(context);
+        init();
     }
 
     public CustomView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public CustomView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
 
-
+    public void init(){
+        inflate(getContext(), R.layout.gpslayout, this);
+    }
 }
--- app/src/main/java/com/example/english/gps_tracker/MainActivity.java
+++ app/src/main/java/com/example/english/gps_tracker/MainActivity.java
@@ -1,16 +1,87 @@
 package com.example.english.gps_tracker;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    Button track;
+    Button mock;
+    LocationManager locationManager;
+    Boolean activated = false;
+    TextView textt;
+    LocationListener locationListener;
+    String locationProvider = LocationManager.GPS_PROVIDER;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        track = (Button)findViewById(R.id.buttont);
+       // mock = (Button)findViewById(R.id.mock);
+        textt = (TextView)findViewById(R.id.textrack);
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                Log.d("DEBUG",location.toString());
+                //makeUseOfNewLocation(location);
+            }
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+                Log.d("DEBUG","changed");}
+
+            public void onProviderEnabled(String provider) {
+                Log.d("DEBUG",locationProvider);
+            }
+
+            public void onProviderDisabled(String provider) {}
+        };
+        track.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(activated){
+                    activated = !activated;
+                    textt.setText("GPS Inactive");
+                    track.setText("Start Tracking");
+                    //locationManager.removeUpdates(locationListener);
+                }else{
+                    activated = !activated;
+                    textt.setText("GPS Active");
+                    track.setText("Stop Tracking");
+                    //locationManager.requestLocationUpdates(locationProvider, 0, 0, locationListener);
+                }
+            }
+        });
+        /*mock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getMockLocation();
+            }
+
+            });*/
+    }
+
+    private void getMockLocation()
+    {
+        Location l = new Location(locationProvider);
+        l.setLatitude(1.2345d);
+        l.setLongitude(1.2345d);
+        l.setAccuracy(0.01f);
+        l.setTime(System.currentTimeMillis());
+        l.setElapsedRealtimeNanos(System.currentTimeMillis());
+        locationManager.addTestProvider(locationProvider, false,false, false, false, false, true, true, 0, 5);
+        locationManager.setTestProviderLocation(locationProvider,l);
     }
 
     @Override
