ebf7d8e703fc7318d97b29da481fb7f257eda4b9
Gendai <t.brenet@orange.fr>
2016-04-20 10:48:48 +0100

    MileStone01

---
+++ app/src/main/java/com/example/english/gps_tracker/CustomView.java
@@ -0,0 +1,23 @@
+package com.example.english.gps_tracker;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+public class CustomView extends View {
+
+    public CustomView(Context context) {
+        super(context);
+    }
+
+    public CustomView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
